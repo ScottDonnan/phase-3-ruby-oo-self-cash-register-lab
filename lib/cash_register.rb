2658ee90efa74c34a7d5cfc1d7d46f405f28cc88
Scott Donnan <scott.donnan@me.com>
2021-10-14 15:06:18 -0500

    commit tree 825a7667d14ae0d1454c75fb15da9794780c7f27

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :discount, :total, :title, :price, :quantity
+    def initialize(total=0, discount=20)
+        @total = total
+        @discount = discount
+    end
+
+    def add_item(title, price, quantity=1)
+        self.title= title
+        self.price= price
+        self.quantity= quantity
+        self.total = @total + @price * @quantity
+    end
+
+    def apply_discount
+        self.total= self.total - (self.discount.to_f/100 * self.total)
+    end
+end
+
+cart = CashRegister.new
+cart.add_item("eggs", 10, 2)
+
+binding.pry
